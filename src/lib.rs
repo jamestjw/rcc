16a7d312ba85f18899c21d1a4143dc899debea6b
James Tan <james_tan97@outlook.com>
2021-06-29 21:28:24 +0800

    Implement basic functionality of a parser and print out AST

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,31 @@
 // All rights reserved.
 
 // This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree. 
+// LICENSE file in the root directory of this source tree.
 
+pub mod debug;
+pub mod parser;
 pub mod scanner;
+
+// TODO: Find a way make it possible remove the requirement to define
+// the specific traits we want to derive within the macro
+#[macro_export]
+macro_rules! enum_str {
+    (#[derive($($trait:ident),*)]
+    pub enum $name:ident {
+        $($variant:ident),*,
+    }) => {
+        #[derive($($trait),*)]
+        pub enum $name {
+            $($variant),*
+        }
+
+        impl $name {
+            pub fn name(&self) -> &'static str {
+                match self {
+                    $($name::$variant => stringify!($variant)),*
+                }
+            }
+        }
+    };
+}
--- src/scanner/token.rs
+++ src/scanner/token.rs
@@ -2,28 +2,29 @@
 // All rights reserved.
 
 // This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree. 
+// LICENSE file in the root directory of this source tree.
+use crate::enum_str;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum TokenType {
-    EOF,
-    PLUS,
-    MINUS,
-    SLASH,
-    STAR,
-    SEMI,
-    INTLIT,
-    LPAREN,
-    RPAREN,
-    IDENT,
-    PRINT, // Temporary until we have function calls
+enum_str! {
+    #[derive(PartialEq)]
+    pub enum TokenType {
+        EOF,
+        PLUS,
+        MINUS,
+        SLASH,
+        STAR,
+        INTLIT,
+        IDENT,
+        LPAREN,
+        RPAREN,
+        PRINT, // Temporary until we have function calls
+        SEMI,
+    }
 }
 
-
-#[derive(Debug)]
 pub struct Token {
     pub token_type: TokenType,
-    pub int_value: u32,
+    pub int_value: i32,
 }
 
 impl Token {
